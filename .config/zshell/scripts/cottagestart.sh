1cd465e48176c344321d4964d5d45ad73fa732c7
Brenton Beltrami <contact@brentonbeltrami.com>
2022-03-24 13:49:18 -0500

    added update

--- .config/zshell/scripts/cottagestart.sh
+++ .config/zshell/scripts/cottagestart.sh
@@ -1,17 +1,3 @@
 #! /bin/zsh
-tmux rename-session "cottage"
-cd ~/cottage/cottage-app
-tmux send-keys "cd ~/cottage/cottage-app" Enter
-tmux send-keys "npm run dev" Enter
-tmux split-window -h
-tmux send-keys "git status" Enter
-tmux select-pane -L
-tmux split-window
-tmux send-keys "npm run prisma-studio" Enter
-tmux split-window
-tmux send-keys "docker compose up" Enter
-tmux select-pane -R
-open -a /Applications/Firefox.app
-open https://github.com/joincottage/cottage-app/projects/1
-#/usr/bin/osascript -e 'tell application "Safari"' -e "activate" -e 'tell application "System Events"' -e 'keystroke "f" using {control down, command down}' -e "end tell" -e "end tell"
+git commit --amend --no-edit --date "$(date '+%a %d %b %Y %H:%M:%S %Z')"
 
