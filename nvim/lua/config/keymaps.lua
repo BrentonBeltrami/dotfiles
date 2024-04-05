b3e173b7962855ca66054ce138406210141c4d10
Brenton Beltrami <contact@brentonbeltrami.com>
2024-04-04 19:40:17 -0500

    updated vimrc

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,7 +3,8 @@
 -- Add any additional keymaps here
 local wk = require("which-key")
 
-vim.api.nvim_set_keymap("n", "<C-f>", ":siletn !tmux neww tmux-sessionizer<CR>", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-f>", ":silent !tmux neww tmux-sessionizer<CR>", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-t>", ":silent !tmux neww todo<CR>", { silent = true })
 
 wk.register({
   z = { name = "Zen Mode", m = { "<cmd>ZenMode<CR>", "Zen mode" } },
